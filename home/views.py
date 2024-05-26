f92e493f163cb2383cbb62c48b92abfc69025a1e
Rahul Seth <sethrahul4u@gmail.com>
2024-05-27 01:30:41 +0530

    Intial Commit

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def dogfood(request):
+    return render(request, 'dogfood.html')
