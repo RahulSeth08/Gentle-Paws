2ad1fc367ea52343d3516bfa8ed5fba1c520da4b
Rahul Seth <sethrahul4u@gmail.com>
2024-07-23 15:31:10 +0530

    bugs fixed

--- GentlePaws/GentlePaws/settings.py
+++ GentlePaws/GentlePaws/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d^^%ib^n^vk^6*%p4ddq9@5^5(gl-z-sl*g-^rh9heg1hg3n5v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/assets/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS = [
